4fa6eb115f72ac20702578db47f556ac14bf74cb
Jacob Singer <jacobleesinger@gmail.com>
2025-03-25 11:02:20 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import 'media-chrome';
+</script>
+
+<media-controller>
+	<!-- svelte-ignore a11y_media_has_caption -->
+	<video slot="media" src="https://stream.mux.com/DS00Spx1CV902MCtPj5WknGlR102V5HFkDe/high.mp4"
+	></video>
+</media-controller>
